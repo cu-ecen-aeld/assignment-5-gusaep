a6430105e20c8f5469657c2f0ea93ad3e72fe78e
gusaep <gustavoaep@gmail.com>
2024-06-25 00:28:38 +0200

    Bump assignment 3 reference

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 632525e0ea65bd5329438d6e802dcc95253db321
+AESD_ASSIGNMENTS_VERSION = 888a30a60f9e9f83fb21c356a9364da18ddd1ac1
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
